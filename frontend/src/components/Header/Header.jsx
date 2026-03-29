ba00633c2eb68a19ee3240d7d4383941a9d8b3a8
Fatima <fatima.arab@gmail.com>
2026-03-28 21:07:39 -0400

    working on css for header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className='header-contents'>
+        <h2>Order Your favorite food here</h2>
+        <p>Choose from a diverse menu featuring delicious list of dishes crafted with care and finest ingredients.</p>
+        <button>View Menu</button>
+       </div>
+    </div>
+  )
+}
+
+export default Header
