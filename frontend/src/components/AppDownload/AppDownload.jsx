5a12b3bdfefbb6f6338abdbaaf65fcfff2bec693
Fatima <fatima.arab@gmail.com>
2026-03-30 21:40:30 -0400

    Home page is done

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AppDownload.css'
+import {assets} from '../../assets/assets'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download' id='app-download'>
+       <p>Download our app for the best ordering experience! <br />Tomato App</p>
+       <div className="app-download-platforms">
+         <img src={assets.app_store} alt="App Store" />
+         <img src={assets.play_store} alt="Play Store" />
+       </div>
+    </div>
+  )
+}
+
+export default AppDownload
