8c2585fc607dce1596ad75967b1ff18e15861925
Fatima <fatima.arab@gmail.com>
2026-04-06 20:18:47 -0400

    admin page started, css for add food done

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Sidebar.css'
+import {assets} from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <NavLink className="sidebar-option" to="/add">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </NavLink>
+
+        <NavLink className="sidebar-option" to="/list">
+          <img src={assets.order_icon} alt="" />
+          <p>list Items</p>
+        </NavLink>
+
+        <NavLink className="sidebar-option" to="/orders">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </NavLink>
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
