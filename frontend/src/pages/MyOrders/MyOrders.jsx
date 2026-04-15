61240ff22bed82295b9d3c3270955c635970258d
Fatima <fatima.arab@gmail.com>
2026-04-14 22:23:53 -0400

    track order functionality added

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -47,7 +47,7 @@ const MyOrders = () => {
                         <p>${order.amount.toFixed(2)}</p>
                         <p>Items: {order.items.length}</p>
                         <p><span>&#x25cf;</span><b>{order.status}</b></p>
-                        <button> Track Order</button>
+                        <button onClick={fetchOrders}> Track Order</button>
                     </div>
                 )
             })}
