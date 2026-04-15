c3759d4f4c878ccf57322fe42f1e402d4bcd432d
Fatima <fatima.arab@gmail.com>
2026-04-15 19:34:06 -0400

    fix VerifyPayment import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-import VerifyPayment from "./pages/VerifyPayment/VerifyPayment";
+import VerifyPayment from "./pages/VerifyPayment/VerifyPayment";//  i made amistake in uppercase and lead to error while deploying
 import MyOrders from "./pages/MyOrders/MyOrders";
 
 const App = () => {
